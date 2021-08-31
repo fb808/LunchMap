b678f0fc294472f317418de610d1faa18c5e5035
fb808 <s2019w26@e-mirim.hs.kr>
2021-08-31 11:23:18 +0900

    코드정리

--- map.js
+++ map.js
@@ -1,23 +1,20 @@
-window.onload = function() {
+// 서비스 실행 시...
+window.onload = function () {
   handleRefresh();
 }
 
-// JSON 파일 로드
-function handleRefresh() {
-    var requestURL = 'http://127.0.0.1:5500/tradingArea.json';
-    $.getJSON(requestURL, NearbyRestaurant);
-    $.getJSON(requestURL);
-}
+// 회사 위치
+let company = { latitude: 37.50764693316519, longitude: 127.05776158879458 };
 
-var company = { latitude: 37.50764693316519, longitude: 127.05776158879458 };
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+// 지도 설정
+let mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = { 
         center: new kakao.maps.LatLng(company.latitude, company.longitude), // 지도의 중심좌표 (회사)
         level: 1 // 지도의 확대 레벨
     };
 
-// 지도를 표시할 div와 지도 옵션으로 지도를 생성
-var map = new kakao.maps.Map(mapContainer, mapOption); 
+// 지도 생성
+let map = new kakao.maps.Map(mapContainer, mapOption); 
 
 // 마커가 표시될 위치 - 회사 위치 넣고 시작
 let markerPosition = [
@@ -31,35 +28,59 @@ let markerPosition = [
 ];
 
 // 마커 이미지 주소
-var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+let imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
 
 // 마커 이미지 크기
-var imageSize = new kakao.maps.Size(30, 45); 
+let imageSize = new kakao.maps.Size(30, 45); 
     
-// 마커 이미지 생합니다    
-var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
-var marker = new kakao.maps.Marker({
+// 마커 이미지   
+let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
+
+// 마커 설정
+let marker = new kakao.maps.Marker({
     map: map, // 마커를 표시할 지도
     position: markerPosition[0].latlng, // 마커를 표시할 위치
     title: markerPosition[0].title, // 마커의 타이틀
     image: markerImage // 마커 이미지
 });
 
+// 회사 마커 생성
 marker.setMap(map);
 
+// 회사 바로가기
+function goHome (){
+    // 이동할 위치
+    let moveLatLon = new kakao.maps.LatLng(company.latitude, company.longitude);
+    // 중심 이동
+    map.setCenter(moveLatLon);
+}
+
+// JSON 파일 로드
+function handleRefresh() {
+    let requestURL = 'http://127.0.0.1:5500/tradingArea.json';
+    $.getJSON(requestURL, NearbyRestaurant);
+}
+
+// JSON 파일에서 필요한 부분만 골라내기
 function NearbyRestaurant(restaurantInfo) {
+
+    // JSON 파일 받기
     let info = restaurantInfo;
 
     for (var i = 0; i < info.length; i++){
-        // 주변 식당 위치값 markerPosition에 저장하기
+        // 필요한 정보만
         var obj = {
             title: info[i].상호명,
-            latlng: new kakao.maps.LatLng(info[i].위도, info[i].경도),
-            distance: computeDistance({latitude: info[i].위도, longitude: info[i].경도}),
+            latlng: new kakao.maps.LatLng(info[i].위도, info[i].경도), // 위경도로 카카오맵 api 마커 생성에 필요한 객체 생성
+            distance: computeDistance({latitude: info[i].위도, longitude: info[i].경도}), // 거리 구하기
             address: info[i].도로명주소,
             classification: info[i].상권업종소분류명
         };
+        
+        // 주변 식당 정보 markerPosition에 저장하기
         markerPosition.push(obj);
+
+        // 데이터 거리순으로 정렬
         markerPosition = markerPosition.sort(function (a, b) {
             return a.distance - b.distance;
         });
@@ -67,57 +88,92 @@ function NearbyRestaurant(restaurantInfo) {
 
     // 마커 생성
     setTimeout(makeMarker(), 300);
-
-    // 리스트 생성
-    list();
 }
 
+// 마커 생성
 function makeMarker() {
     for (var i = 1; i < markerPosition.length; i++) {
-        var restaurantMarker = new kakao.maps.Marker({
-            map: map, // 마커를 표시할 지도
-            position: markerPosition[i].latlng, // 마커를 표시할 위치
-            title: markerPosition[i].title // 마커의 타이틀
-        });
-        restaurantMarker.id = markerPosition[i].title;
-        // 지도 위에 마커 표시
-        restaurantMarker.setMap(map);
-
-        kakao.maps.event.addListener(restaurantMarker, 'click', function() {
-            alert(this.id);
-            document.getElementById(this.id).scrollIntoView();
-        });
+        // 마커 초기화
+        showMarker(i);
+        // 리스트 생성
+        list(i);
     }
 }
 
-function list() {
-    let restaurantInfoDiv = document.getElementById('list');
-    for (var i = 1; i < markerPosition.length; i++) {
-        var mDistance = markerPosition[i].distance;
-        mDistance >= 1000 ? mDistance = mDistance / 1000 + 'km' : mDistance += 'm';
-        // 주변 식당 정보
-        var div = document.createElement('div');
-        div.setAttribute('class', 'restaurantInfoItem');
-        div.setAttribute('id', markerPosition[i].title);
-        div.innerHTML = `<b>${markerPosition[i].title}</b><br>
-                        ${mDistance}<br>
-                        ${markerPosition[i].classification}<br>
-                        ${markerPosition[i].address}`;
-        restaurantInfoDiv.appendChild(div);
+// 마커 정보 저장용
+let restaurantMarker;
 
-        document.getElementById(markerPosition[i].title).onclick = function () {
-            var index = markerPosition.findIndex(p => p.title == this.id);
-            // 이동할 위도 경도 위치를 생성합니다
-            var moveLatLon = markerPosition[index].latlng;
-            // 지도 중심을 이동 시킵니다
-            map.setCenter(moveLatLon);
-            // document.getElementById(markerPosition[index].title).scrollIntoView();
-        };
-    }
+// 마커 초기화
+function showMarker(i) {
+
+    // 마커 정보
+    restaurantMarker = new kakao.maps.Marker({
+        position: markerPosition[i].latlng, // 마커를 표시할 위치
+        title: markerPosition[i].title, // 마커의 타이틀
+    });
+
+    // 마커 아이디값 지정
+    restaurantMarker.id = markerPosition[i].title;
+
+    // 맵 위에 마커 보이게
+    setMarker(map);
+
+    // 마커 클릭 이벤트
+    kakao.maps.event.addListener(restaurantMarker, 'click', function() {
+        alert(this.id);
+        document.getElementById(this.id).scrollIntoView();
+    });
 }
 
-// 구면 코사인 법칙(Spherical Law of Cosine) 으로 두 위도/경도 지점의 거리를 구함
-// 반환 거리 단위 (m)
+// 맵 위에 마커 보이게
+function setMarker(map) {
+    restaurantMarker.setMap(map);
+}
+
+// 맵 위에 마커 안보이게... 라는데 해본 적 X
+function hideMarker(condition) {
+    setMarker(null);
+}
+
+// 리스트 생성할 위치
+let restaurantInfoDiv = document.getElementById('list');
+
+// 리스트 생성
+function list(i) {
+
+    // 거리 단위 변환
+    let mDistance = markerPosition[i].distance;
+    mDistance >= 1000 ? mDistance = mDistance / 1000 + 'km' : mDistance += 'm';
+    
+    // 리스트 class, id 값 지정
+    var div = document.createElement('div');
+    div.setAttribute('class', 'restaurantInfoItem');
+    div.setAttribute('id', markerPosition[i].title);
+
+    // 리스트 출력
+    div.innerHTML = `<b>${markerPosition[i].title}</b><br>
+                    ${mDistance}<br>
+                    ${markerPosition[i].classification}<br>
+                    ${markerPosition[i].address}`;
+    
+    // 출력 위치
+    restaurantInfoDiv.appendChild(div);
+
+    // 리스트 클릭 이벤트
+    document.getElementById(markerPosition[i].title).onclick = function () {
+
+        // id 값이 같은 마커 찾기
+        var index = markerPosition.findIndex(p => p.title == this.id);
+
+        // 이동
+        var moveLatLon = markerPosition[index].latlng;
+        map.setCenter(moveLatLon);
+    };
+}
+
+
+
+// 구면 코사인 법칙(Spherical Law of Cosine) 으로 두 위도/경도 지점의 거리를 구함, 반환 거리 단위 (m)
 function computeDistance(destCoords) {
     var startCoords = company;
     var startLatRads = degreesToRadians(startCoords.latitude);
@@ -136,11 +192,4 @@ function computeDistance(destCoords) {
 function degreesToRadians(degrees) {
     radians = (degrees * Math.PI)/180;
     return radians;
-}
-
-function goHome (){
-    // 이동할 위도 경도 위치를 생성합니다
-    var moveLatLon = new kakao.maps.LatLng(company.latitude, company.longitude);
-    // 지도 중심을 이동 시킵니다
-    map.setCenter(moveLatLon);
 }
