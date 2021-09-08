47824d3d18483f20b4777164596d8c58d2f1d10e
fb808 <s2019w26@e-mirim.hs.kr>
2021-09-08 18:24:45 +0900

    크롤링한거 디비에 넣기

--- crawling.py
+++ crawling.py
@@ -1,31 +1,56 @@
 from selenium import webdriver
 import time
 import json
+import pymysql
+
+lunchMap_db = pymysql.connect(
+    user='ryu',
+    passwd='aqswde123',
+    host='localhost',
+    db='LunchMap',
+    charset='utf8'
+)
+
+curs = lunchMap_db.cursor()
 
 with open('./public/tradingArea.json') as f:
     json_object = json.load(f)
 
 driver = webdriver.Chrome("chromedriver")
 
-for obj in [json_object[0], json_object[1], json_object[2], json_object[3], json_object[4], json_object[5]]:
+for obj in json_object:
+
+    no = obj['상가업소번호']
     title = obj['상호명']
-    if obj['법정동코드'] == '1168010500':
-        title = '삼성동 ' + obj['상호명']
-    elif obj['법정동코드'] == '1168010600':
-        title = '대치동 ' + obj['상호명']
+    kindCode = obj['상권업종중분류코드']
+    classification = obj['상권업종소분류명']
+    address = obj['도로명주소']
+    latitude = float(obj['위도'])
+    longitude = float(obj['경도'])
+
     print(title)
 
     try:
-        kakao_map_search_url = f"https://map.kakao.com/?q={title}"
+        searchTitle = title
+        if obj['법정동코드'] == '1168010500':
+            searchTitle = '삼성동 ' + obj['상호명']
+        elif obj['법정동코드'] == '1168010600':
+            searchTitle = '대치동 ' + obj['상호명']
+        
+        kakao_map_search_url = f"https://map.kakao.com/?q={searchTitle}"
         driver.get(kakao_map_search_url)
         time.sleep(1)
 
         rate = driver.find_element_by_xpath("/html/body/div[5]/div[2]/div[1]/div[7]/div[5]/ul/li[1]/div[4]/span[1]/em").text
 
         print("평점 " + str(rate))
+        
+        sql = f'''INSERT INTO `Restaurant` VALUES ({no}, '{title}', '{kindCode}', '{classification}', '{address}', {latitude}, {longitude}, {rate})'''
+        curs.execute(sql)
+        lunchMap_db.commit()
 
     except Exception as e1:
         print(e1)
-        print("정보 없음")
         pass
-    
+
+lunchMap_db.close()
