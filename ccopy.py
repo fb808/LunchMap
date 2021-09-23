a499ed9713e70110d3bbc3e66ca5bc33994c203e
fb808 <s2019w26@e-mirim.hs.kr>
2021-09-23 12:26:58 +0900

    키워드줄이기

--- ccopy.py
+++ ccopy.py
@@ -23,22 +23,30 @@ for idx, obj in df.iterrows():
             cate4_d.append('국/탕')
         elif tag == '국수' or tag == '냉면':
             cate4_d.append('국수')
-        elif tag == '육류,고기' or tag == '불고기,두루치기' or tag == '삽겹살' or tag == '갈비':
-            cate4_d.append('육류,고기')
+        elif tag == '육류,고기' or tag == '불고기,두루치기' or tag == '족발,보쌈'or tag == '삼겹살' or tag == '갈비' or tag == '닭요리' or tag == '오리':
+            cate4_d.append('육류/고기')
         elif tag == '곱창,막창' or tag == '순대':
-            cate4_d.append('곱창,막창,순대')
+            cate4_d.append('곱창/막창/순대')
         elif tag == '치킨' or tag == '닭강정':
             cate4_d.append('치킨')
-        elif tag == '닭요리' or tag == '오리':
-            cate4_d.append('닭/오리')
         elif tag == '해물,생선' or tag == '회' or tag == '장어' or tag == '게,대게' or tag == '복어' or tag == '참치회' or tag == '아구' or tag == '조개':
-            cate4_d.append('해물,생선')
-        elif tag == '일식' or tag == '일식집' or tag == '퓨전일식' or tag == '일본식라면' or tag == '초밥,롤':
-            cate4_d.append('일식')
-        elif tag == '분식' or tag == '떡볶이' or tag == '주먹밥':
+            cate4_d.append('해물/생선')
+        elif tag == '일식' or tag == '일식집' or tag == '퓨전일식' or tag == '일본식라면' or tag == '초밥,롤' or tag == '돈까스,우동':
+            cate4_d.append('일식/돈까스')
+        elif tag == '분식' or tag == '떡볶이' or tag == '주먹밥' or tag == '오뎅바':
             cate4_d.append('분식')
         elif tag == '패스트푸드' or tag == '도시락' or tag == '샌드위치' or tag == '햄버거' or tag == '피자' or tag == '토스트':
             cate4_d.append('패스트푸드')
+        elif tag == '양식' or tag == '스테이크,립' or tag == '이탈리안' or tag == '패밀리레스토랑' or tag == '샐러드':
+            cate4_d.append('양식')
+        elif tag == '중식' or tag == '중화요리' or tag == '퓨전중식':
+            cate4_d.append('중식')
+        elif tag == '치킨' or tag == '닭강정':
+            cate4_d.append('치킨')
+        elif tag == '베트남음식' or tag == '아시아음식' or tag == '동남아음식':
+            cate4_d.append('아시아음식')
+        else:
+            cate4_d.append('기타')
     except Exception as e1:
         print(e1)
         pass
